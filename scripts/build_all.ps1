f5929058e13d36977d6dd5a239c3f7c770a32106
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-20 13:13:08 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,9 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
-  -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -Version "1.0.0" `
+  -OutputFilePrefix "B66_Alpha" `
+  -ModName "Bunker 66 Alpha" `
+  -ModFolder "B66" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/bunker66/B66-Alpha-Original-Cleaned.zip" `
+  -ModBaseFilesUrlHash "9833719596d6c326df4335fcdb7707e5d40f44e99a856d7c6adc3788ba9455e4" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
